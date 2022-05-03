5086cf7dfca6da67ee96a1a664bc82a4dc7b652d
Lehrbas <eduardo-lehrbach@hotmail.com>
2022-05-03 20:08:24 -0300

    Added transaction, block and hash methods

--- bin/blockchain.py
+++ bin/blockchain.py
@@ -1,20 +1,52 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+        # Create genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_block(self, proof, previous_hash=None):
+        # Creates/init block
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Resets current transactions
+
+        self.current_transactions = []
+
+        # Append block to the chain and return it
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        # Sender and recipient are strings, addresses
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        # Creates SHA-256 hash of a Block
+        # Block is a <dict> param
+        
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
