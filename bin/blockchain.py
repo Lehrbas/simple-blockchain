855620b4bcaf8a26cc8e1d9737268a8eb7980543
Lehrbas <eduardo-lehrbach@hotmail.com>
2022-05-03 21:21:42 -0300

    Add basic proof of work

--- bin/blockchain.py
+++ bin/blockchain.py
@@ -40,6 +40,17 @@ class Blockchain(object):
         })
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        # Simple PoW algorithm
+        # Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        # p is the previous proof, and p' is the new proof
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        
+        return proof
+
     @staticmethod
     def hash(block):
         # Creates SHA-256 hash of a Block
@@ -48,6 +59,14 @@ class Blockchain(object):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        # Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+        
     @property
     def last_block(self):
         # Returns the last Block in the chain
